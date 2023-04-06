a4dff967125f282589bd4294c8f7e3f1867d0fb7
zoccojulian <zoccojulian@gmail.com>
2023-04-06 19:42:42 -0300

    inicio

---
+++ src/component/InView.jsx
@@ -0,0 +1,31 @@
+import React, { useRef, useEffect } from 'react';
+import { motion, useInView } from 'framer-motion';
+
+
+
+
+const InView = () => {
+
+    const referencia = useRef(null)
+    const isInView = useInView(referencia)
+
+    useEffect(() => {
+        console.log("Element is in view: ", isInView)
+      }, [isInView])
+
+    return (
+        <div className='container' ref={ referencia }>
+            {isInView && 
+                <motion.img 
+                    src='https://www.icegif.com/wp-content/uploads/2022/01/icegif-1479.gif'
+                    initial={{ opacity:0, transform:'translateX(-100px)'}}
+                    animate={{opacity:1, transform:'translateX(0px)'}}
+                    // exit={{opacity:0}}
+                    transition={{delay:1}}
+                ></motion.img>
+            }
+        </div>
+    );
+}
+
+export default InView;
---
+++ src/component/Parallax.jsx
@@ -0,0 +1,59 @@
+import React, { useState , useRef, useEffect} from 'react';
+import { color, motion , useMotionValueEvent, useScroll, useSpring, useTransform} from 'framer-motion';
+
+const Parallax = () => {
+
+    const useParallax = ( value ,arr, d1, d2) => {
+        return useTransform(value,arr,[d1, d2])
+    }
+    const referencia = useRef(null);
+    const {scrollYProgress} = useScroll({target: referencia})
+    const movimientoenX = useParallax(scrollYProgress,[1,0.7],'-50vw','0vw')
+    const [movX, setMovX] = useState('0vw');
+
+    const movimientoX2 = useParallax(scrollYProgress,[0.7,0.2],'50vw','0vw');
+    const [mov2x, setMov2x] = useState('0vw')
+
+    // const scrollX = useSpring(scrollYProgress);
+
+
+    useMotionValueEvent(scrollYProgress, "change", (ev) => {
+        setMovX(movimientoenX.current)
+        setMov2x(movimientoX2.current)
+        // console.log(movimientoenX.current)
+        
+    })
+
+
+    return (
+        <div className='container' >
+            <motion.h1
+                className='texto'
+                initial={{x:-200}}
+                animate={{x:0}}
+                transition={{duration: 1, type:'spring'}}
+                ref={referencia}
+            >hola</motion.h1>
+            <motion.h2
+                className='nombre'
+                style={{transform: `translateX(${movX})`}}
+            >
+            Julian Zocco
+            </motion.h2>
+            <motion.img 
+                    src='https://www.icegif.com/wp-content/uploads/2022/01/icegif-1479.gif'
+                    // initial={{ opacity:0}}
+                    // animate={{opacity:1}}
+                    style={{transform: `translateX(${mov2x})`}}
+                    transition={{delay:1}}
+                ></motion.img>
+            <motion.div
+                className='caja'
+                // style={{scaleX: scrollX}}
+            >
+            </motion.div>
+        </div>
+    );
+}
+
+export default Parallax;
